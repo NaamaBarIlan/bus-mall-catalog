5a475de363e5bdbfd1f3cab40bc07d907e3c8f4e
NaamaBarIlan <barilan.naama@gmail.com>
2019-05-16 15:02:09 -0700

    Added addItem method

--- js/catalog.js
+++ js/catalog.js
@@ -9,7 +9,7 @@ var cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //Added an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   for (var i = 0; i < Product.allProducts.length; i++) {
     var optionEl = document.createElement('option');
